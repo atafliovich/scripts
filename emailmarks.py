8189c928fb5fb482beec3759621d315484286652
Anya Tafliovich <atafliovich@utsc.utoronto.ca>
2016-01-20 10:19:45 -0500

    fixed up emailmarks

--- emailmarks.py
+++ emailmarks.py
@@ -6,24 +6,21 @@ import argparse
 import os
 import sys
 import time
-from util import Student
-from util import load_bb
+from utils import Student
+from utils import load_bb
 
-course = "CSC C24"
-asst = "Lab 1"
-bbfile = "/cmshome/tafliovi/send/jan7.csv"
-path_prefix = "/cmshome/tafliovi/send/lab01"
-path_suffix = ""
+subject = "CSC C24: grading results for Lab01"
+bbfile = "/cmshome/tafliovi/c24/jan7.csv"
+path_prefix = "/cmshome/tafliovi/c24/submissions"
+path_suffix = "lab01/result.txt"
 
-sender = "atafliovich@utsc.toronto.edu"
+sender = "atafliovich@utsc.utoronto.ca"
 sendmail_loc = "/usr/sbin/sendmail"
-subject = "%s: grading results for %s" % (course, asst)
-
 
 def send_mail(recipient, subject, message_body):
     """(str, str, str) -> NoneType
     
-    Send an email to recepien with subject line subject
+    Send an email to recepient with subject line subject
     and message body message_body.
     """
     
@@ -40,9 +37,9 @@ def send_mail(recipient, subject, message_body):
 def send_mails(students, subject, path_pref, path_suff):
     """({str: Student}, str, str, str) -> NoneType
     
-    Send an email to each student in the dictionary of Students (by student_id),
-    with subject subject and the message body being the contents of a file
-    path_pref/Student.utorid/path_suff.
+    Send an email to each student in the dictionary of Students (by
+    student_id), with subject subject and the message body being the
+    contents of a file path_pref/Student.utorid/path_suff.
     """
 
     for student in students.values():
@@ -55,18 +52,28 @@ def send_mails(students, subject, path_pref, path_suff):
 
             send_mail(student.email, subject, body)
         except IOError as error:
-            print("No result for %s: %s" % student.student_id, error)
+            print("No result for %s: %s" % (student.student_id, error))
 
 
 if __name__ == "__main__":
 
     # get args
     parser = argparse.ArgumentParser(
-        description=('Email contents of result files to students.'))
-    parser.add_argument('classlist',
+        description=('Email contents of result files to students.\n' +
+                     'If an optional arg is not specified, the value defined at the top of this file is used.'))
+    parser.add_argument('--subject',
+                        help='The subject line.')
+    parser.add_argument('--classlist',
                         help='Path to the classlist file in BB format.')
+    parser.add_argument('--path_prefix',
+                        help='Prefix of the path to the file to email: up to student submission directory.')
+    parser.add_argument('--path_suffix',
+                        help='Sufffix of the path to the file to email: from the student submission directory.')
     args = parser.parse_args()
 
-    # email
-    students = load_bb(args.classlist)
-    send_mails(students, subject, path_prefix, path_suffix)
+    # email        
+    students = load_bb(open(args.classlist), False) if args.classlist else load_bb(open(bbfile), False)
+    send_mails(students, 
+               subject, 
+               args.path_prefix if args.path_prefix else path_prefix,
+               path_suffix if args.path_suffix else path_suffix)
