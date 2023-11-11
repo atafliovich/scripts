069ee330178775cb343e3226ffea6ffeeb29a3d8
Anya Tafliovich <anya@cs.utoronto.ca>
2023-11-11 16:51:24 -0500

    new version of MarkUs, new structure of config files

--- testers/grade.py
+++ testers/grade.py
@@ -1,161 +1,254 @@
-""" Work with MarkUs.
-"""
+"""Autograde assignment."""
 
+import os
+import string
 import sys
 import canvasapi
 import markusapi
 
-import gradeutils
-import markusutils
+from publicconfig import (
+    MARKUS_URL, QUERCUS_URL, MARKUS_COURSE_ID, QUERCUS_COURSE_ID)
+from privateconfig import (
+    SCRIPTS_DIR, COURSE_DIR, MARKUS_API_KEY, QUERCUS_API_KEY)
 
-sys.path.append('/home/anya/scripts')  # NOQA: E402
+sys.path.append(SCRIPTS_DIR)  # noqa
 from admin import students  # noqa
 from admin import gradebook as gb  # noqa
 from canvas import utils as canvasutils  # noqa
+from uamutils import gradeutils  # noqa
+from markusutils import markusutils  # noqa
 
-MARKUS_API_KEY = ''
-QUERCUS_API_KEY = ''
-QUERCUS_COURSE_ID = ''
 
-COURSE = 'cscc24w21'
-CONFIG = 'config.py'
+CONFIG = 'a2config.py'
 
-ASSIGNMENT_ID = 12
-ASSIGNMENT_NAME = 'lab8'
+ASSIGNMENT_ID = 99
+ASSIGNMENT_NAME = 'a2'
 
-MARKING_DIR = '/home/anya/c24/labs/08/marking'
+MARKING_DIR = os.path.join(COURSE_DIR, 'assignments', 'a2', 'marking')
 LOCAL_DIR = 'submissions'
 
 # paths relative to student repo directory (utorid on MarkUs)
-MARKUS_FILES = ['Lab8.hs']
-LOCAL_FILES = ['Lab8.hs']
+MARKUS_FILES = ['bridge_functions.py']
+LOCAL_FILES = ['bridge_functions.py']
 
-RESULT_FILES = {  # without exptension
+TEMPLATE_FILE = 'individual_short.tpl'
+RESULT_FILES = {  # without extension
     'result'
 }
 
 RESULT_FILES_TO_WEIGHTS = {'result': 'weights.gf'}
 
-UPLOAD_FILES = {  # with exptension
-    'output.txt',
-    'result.txt'
+UPLOAD_FILES = {  # with extension
+    'result.txt',
+    'pyta_output.txt'
 }
 
-GF_MARKS = {"apply": 4, "element": 5, "replace": 5, "sumLeafs": 4,
-            "numNodes": 3, "numLevels": 3, "flip": 3, "hlint": 1}
-CRITERIA = {"apply": 2, "element": 2, "replace": 2, "sumLeafs": 2,
-            "numNodes": 2, "numLevels": 2, "flip": 3, "hlint": 3}
+GF_MARKS = {  # TODO
+    'get_bridge': 1,
+    'get_average_bci': 1,
+    'get_total_length_on_hwy': 1,
+    'get_distance_between': 1,
+    'get_closest_bridge': 1,
+    'get_bridges_in_radius': 1,
+    'get_bridges_with_bci_below': 1,
+    'get_bridges_containing': 1,
+    'assign_inspectors': 1,
+    'inspect_bridges': 1,
+    'add_rehab': 1,
+    'format_data': 1,
+    'doctests': 1,
+    'docstrings': 1,
+    'constants': 1,
+    'pyta': 20
+}
+
+CRITERIA = {
+    'get_bridge': 2,
+    'get_average_bci': 2,
+    'get_total_length_on_hwy': 2,
+    'get_distance_between':  2,
+    'get_closest_bridge':  4,
+    'get_bridges_in_radius':  4,
+    'get_bridges_with_bci_below':  4,
+    'get_bridges_containing':  2,
+    'assign_inspectors':  6,
+    'inspect_bridges':  3,
+    'add_rehab':  3,
+    'format_data':  6,
+    'doctests': 3,
+    'docstrings': 3,
+    'constants': 4,
+    'pyta': 10
+}
+
 
 ########################################################################
 # MarkUs
-URL = 'https://markus.utsc.utoronto.ca/{}'.format(COURSE)
-API = markusapi.Markus(MARKUS_API_KEY, URL)
+API = markusapi.Markus(MARKUS_API_KEY, MARKUS_URL)
 
 # Quercus
-Q_API_URL = 'https://q.utoronto.ca'
-CANVAS = canvasapi.Canvas(Q_API_URL, QUERCUS_API_KEY)
+CANVAS = canvasapi.Canvas(QUERCUS_URL, QUERCUS_API_KEY)
 COURSE = CANVAS.get_course(QUERCUS_COURSE_ID)
 
 # submissions info
-GROUPS = API.get_groups(ASSIGNMENT_ID)
-UTORID_TO_GROUP = markusutils.get_utorid_to_group(API, ASSIGNMENT_ID)
+GROUPS = API.get_groups(MARKUS_COURSE_ID, ASSIGNMENT_ID)
+UTORID_TO_GROUP = markusutils.get_utorid_to_group(
+    API, MARKUS_COURSE_ID, ASSIGNMENT_ID)
+UTORIDS = UTORID_TO_GROUP.keys()
+
+TA_UTORIDS = []
+
+GF_DATA = (GF_MARKS,)
+GF_TO_CRITERIA_FUNC_NAMES = dict(zip(GF_MARKS.keys(), GF_MARKS.keys()))
+
+#############################################################################
 
 # map function-name-in-gf-file to
 # (function-name-markus-criteria, gf-out-of, markus-out-of)
 CRITERIA_MAPS = [
-    {name: (name, GF_MARKS[name], CRITERIA[name])
+    {name: (GF_TO_CRITERIA_FUNC_NAMES[name],
+            GF_MARKS[name],
+            CRITERIA[GF_TO_CRITERIA_FUNC_NAMES[name]])
      for (name, outof) in criteria.items()}
-    for criteria in (GF_MARKS,)]
+    for criteria in GF_DATA]
 
 ########################################################################
 
 
-def get_submissions():
-    markusutils.get_submissions(API, ASSIGNMENT_ID, MARKUS_FILES, LOCAL_DIR,
-                                LOCAL_FILES)
+def get_submissions(group='students'):
+    """Get student/TA submissions from MarkUs."""
+
+    markusutils.get_submissions(
+        API, MARKUS_COURSE_ID, ASSIGNMENT_ID,
+        MARKUS_FILES, LOCAL_DIR, LOCAL_FILES,
+        UTORIDS if group == 'students' else TA_UTORIDS)
 
 
-def setup_tester():
-    gradeutils.setup_tester(COURSE, MARKING_DIR, LOCAL_DIR)
+def setup_tester(target_group='student'):
+    """Setup autotester files."""
+
+    gradeutils.setup_tester(COURSE, MARKING_DIR, LOCAL_DIR, target_group)
 
 
-def test_all(run, aggregate, template, gen):
-    gradeutils.test_all(MARKING_DIR, LOCAL_DIR, CONFIG, ASSIGNMENT_NAME,
-                        RESULT_FILES, RESULT_FILES_TO_WEIGHTS,
-                        run, aggregate, template, gen)
+def test_all(run: bool, aggregate: bool, template: bool, gen: bool):
+    """Test all submissions."""
+
+    gradeutils.test_all(
+        MARKING_DIR, LOCAL_DIR, CONFIG,
+        ASSIGNMENT_NAME, RESULT_FILES, RESULT_FILES_TO_WEIGHTS,
+        run, aggregate, template, gen, TEMPLATE_FILE)
 
 
-def upload_files():
-    for UPLOAD_FILE in UPLOAD_FILES:
+def upload_all_files(group='students'):
+    """Upload result files to MarkUs."""
+
+    for upload_file in UPLOAD_FILES:
         markusutils.upload_result_files(
-            API, ASSIGNMENT_ID, LOCAL_DIR, UPLOAD_FILE)
+            API, MARKUS_COURSE_ID, ASSIGNMENT_ID, LOCAL_DIR, upload_file,
+            TA_UTORIDS if group == 'tas' else UTORIDS)
 
 
-def upload_grades():
-    for (RESULT_FILE, CRITERIA_MAP) in zip(RESULT_FILES, CRITERIA_MAPS):
-        with open('{}.gf'.format(RESULT_FILE)) as GF_FILE:
+def upload_all_grades(group='students'):
+    """Upload grades to MarkUs."""
+
+    for (result_file, criteria_map) in zip(RESULT_FILES, CRITERIA_MAPS):
+        with open(f'{result_file}.gf', encoding='UTF-8') as gf_file:
             markusutils.upload_grades(
-                API, ASSIGNMENT_ID, GF_FILE, CRITERIA_MAP, False)
+                API, MARKUS_COURSE_ID, ASSIGNMENT_ID,
+                gf_file, criteria_map, False)
 
 
-def test_one(utorid, redownload=False, run=True, aggregate=True,
-             template=True, gen=True, upload_grades=True, upload_files=True):
+def test_one(utorid, redownload=False,
+             run=True, aggregate=True, template=True, gen=True,
+             upload_grades=True, upload_files=True,
+             template_file=TEMPLATE_FILE):
     """Test one submission."""
 
     if redownload:
-        print('Redownloading {}...'.format(utorid))
-        markusutils.get_submission(API, ASSIGNMENT_ID, MARKUS_FILES, LOCAL_DIR,
-                                   LOCAL_FILES, utorid)
+        print(f'Redownloading {utorid}...')
+        markusutils.get_submission(
+            API, MARKUS_COURSE_ID, ASSIGNMENT_ID,
+            MARKUS_FILES, LOCAL_DIR, LOCAL_FILES, utorid)
         gradeutils.setup_tester(COURSE, MARKING_DIR, LOCAL_DIR)
         print('Done redownloading.')
 
-    gradeutils.test_one(utorid, MARKING_DIR, LOCAL_DIR, CONFIG,
-                        ASSIGNMENT_NAME, RESULT_FILES,
-                        RESULT_FILES_TO_WEIGHTS)
+    gradeutils.test_one(
+        utorid, MARKING_DIR, LOCAL_DIR, CONFIG,
+        ASSIGNMENT_NAME, RESULT_FILES, RESULT_FILES_TO_WEIGHTS,
+        run, aggregate, template, gen, template_file)
 
     if upload_grades:
-        for (RESULT_FILE, CRITERIA_MAP) in zip(RESULT_FILES, CRITERIA_MAPS):
-            print('Uploading grades {} for {}...'.format(RESULT_FILE, utorid))
-            with open('{}.gf'.format(RESULT_FILE)) as GF_FILE:
+        for (result_file, criteria_map) in zip(RESULT_FILES, CRITERIA_MAPS):
+            print(f'Uploading grades {result_file} for {utorid}...')
+            with open(f'{result_file}.gf',  encoding='utf8') as gf_file:
                 markusutils.upload_grade(
-                    API, ASSIGNMENT_ID, CRITERIA_MAP,
-                    utorid, None, GF_FILE, None, False)
+                    API, MARKUS_COURSE_ID, ASSIGNMENT_ID, criteria_map,
+                    utorid, None, gf_file, None, False)
             print('Done.')
 
     if upload_files:
-        print('Uploading files for {}...'.format(utorid))
-        for UPLOAD_FILE in UPLOAD_FILES:
-            markusutils.upload_result_file(API,
-                                           ASSIGNMENT_ID,
-                                           LOCAL_DIR,
-                                           UPLOAD_FILE,
-                                           utorid)
+        print(f'Cleaning up output files for {utorid}...')
+        clean_output_files(utorid)
+        print(f'Uploading files for {utorid}...')
+        for upload_file in UPLOAD_FILES:
+            markusutils.upload_result_file(
+                API, MARKUS_COURSE_ID, ASSIGNMENT_ID, LOCAL_DIR,
+                upload_file, utorid)
 
 
 def clean_repos():
-    for utorid, group in UTORID_TO_GROUP.items():
-        for UPLOAD_FILE in UPLOAD_FILES:
-            API.remove_file_from_repo(ASSIGNMENT_ID, group['id'], UPLOAD_FILE)
+    """Remove result files from all repos on MarkUs."""
 
+    for group in UTORID_TO_GROUP.values():
+        for upload_file in UPLOAD_FILES:
+            API.remove_file_from_repo(
+                MARKUS_COURSE_ID, ASSIGNMENT_ID, group['id'], upload_file)
+
+
+def clean_output_files(utorid=None):
+    """Remove special charcaters from result files, because MarkUs can
+    reject them.
+
+    """
+
+    printable = set(string.printable)
+    utorids = [utorid] if utorid else UTORIDS
+    for utid in utorids:
+        for output_file in UPLOAD_FILES:
+            try:
+                with open(f'submissions/{utid}/{output_file}',
+                          encoding='utf8') as orig_f:
+                    content = ''.join(
+                        [char for char in orig_f.read() if char in printable])
+                    with open(f'submissions/{utid}/{output_file}',
+                              'w', encoding='utf8') as new_f:
+                        new_f.write(str(content))
+            except FileNotFoundError:
+                print(f'Warning: no result file for {utid}')
+                continue
 
 # clean_repos()
 
-# with open('criteria_auto.yml', 'w') as outfile:
-#     gradeutils.write_criteria_file(CRITERIA, outfile)
 
+# get_submissions('tas')
 # get_submissions()
 
 # setup_tester()
+# setup_tester('ta')
 
-# test_all(True, True, True, False)
-# test_all(False, False, False, True)
+# with open('criteria.yml', 'w', encoding='utf-8') as outfile:
+#     gradeutils.write_criteria_file(CRITERIA, outfile)
 
-# upload_files()
+# test_all(run, aggregate, template, gen)
+# test_all(True, False, False, False)
+# test_all(False, True, True, True)
 
-# upload_grades()
+# clean_output_files()
+# upload_files('tas')
+# upload_grades('tas')
+# upload_all_files()
+# upload_all_grades()
 
-# test_one('franc272', redownload=False, run=True, aggregate=True,
-#          template=True, gen=True, upload_grades=True, upload_files=True)
 
-# test_one('saqeebna', redownload=True, run=False, aggregate=False,
-#          template=False, gen=False, upload_grades=False, upload_files=False)
+# test_one('tafliovi', redownload=False, run=True, aggregate=False,
+#         template=False, gen=False, upload_grades=False, upload_files=False)
